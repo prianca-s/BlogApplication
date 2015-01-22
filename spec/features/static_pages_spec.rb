436255624efa2450e8b63f218bfdfede121a9014
project1 <psprianca25@gmail.com>
2015-01-22 16:54:02 +0530

    First commit

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", :type => :request do
+  describe "GET /static_pages" do
+ 	#describe "home_Page" do
+ 	#	before { visit root_path } #before expectations
+ 	#	it 'should have right content' do 
+ 	#		page.should have_content('Home Page')  #by capybara
+ 	#	end
+ 	#end
+  end
+end
